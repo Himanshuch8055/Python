78cae8387900679764600217a3228952dad2d4ec
Himanshu Chauhan <himanshuch8055@gmail.com>
2022-08-18 22:49:09 +0530

    first commit

---
+++ Learn Python in One Video.py
@@ -0,0 +1,177 @@
+print("Hello World");
+
+# Hello Himanshu Chauhan how are you
+
+'''Om
+kya
+kar
+rha
+hai'''
+
+sttr = "This is string"
+age = 22
+weight = 75.3
+
+print(sttr)
+print(age)
+print(weight)
+
+# type of opreter
+
+print(type(sttr))
+print(type(age))
+print(type(weight))
+
+# Number Strings Lists Tuple Dictionaries
+
+# Arithmetic Operations
+
+print("The value of 3 + 5 = ", 3 + 5)
+print("The value of 3 - 5 = ", 3 - 5)
+print("The value of 3 * 5 = ", 3 * 5)
+print("The value of 3 / 5 = ", 3 / 5)
+print("The value of 3 ** 5 = ", 3 ** 5)
+print("The value of 3 // 5 = ", 3 // 5)
+
+print("This is Double Cot \"")
+
+mls = '''This 
+        is 
+        Multiline
+        string
+        and
+        this
+        will
+        keep
+        going..............'''
+print(mls)
+
+print("%s to the right "%('This is a string'))
+
+print('this is print statement 1', end=" ")
+print('this is print statement 2')
+
+print("This "*10)
+
+
+# lists
+
+colleges = ['IIT', 'NIT', 'College of engineering']
+
+print(colleges[0])
+print(colleges[1])
+print(colleges[2])
+
+colleges[2] = "COE"
+print(colleges[2])
+print(colleges)
+
+print(colleges[1:3])
+
+list2 = ['table', 'chair', 'fan', 'clothes', 'Bottle']
+print(list2)
+
+list2.append('micophone')
+print(list2)
+list2.insert(3, 'Apple')
+print(list2)
+list2.remove('Apple')
+print(list2)
+
+print(list2 + ['pillow', 'tubelight', 'bed'])
+print(list2)
+
+print(len(list2))
+print(max(list2))
+print(min(list2))
+
+
+# Tuples
+
+
+tup1 = (1, 2, 3)
+# tup1[0] = 6 # TypeError: 'tuple' object does not support item assignment
+print(tup1[0])
+
+# How to Change tuples into list
+
+tup2 = (4, 5, 6)
+list1 = list(tup2)
+print(list1)
+list1[1] = "10"
+print(tup2[0])
+print(list1)
+
+print(len(tup2))
+print(max(tup2))
+print(min(tup2))
+
+print("-------------------------------")
+# Dictionaries
+
+names = {'Himanshu': 21,
+         'umesh': 15,
+         'abhishek': 23,
+         'ankit': 18}
+
+print(names['Himanshu'])
+names['Himanshu'] = 55
+print(names['Himanshu'])
+
+print(names.keys())
+print(names.values())
+
+print("-------------------------------")
+
+# if elseif else statements
+
+print("Enter Your Marks :)")
+numbers = int(input())
+print(numbers)
+
+if(numbers>90):
+    grade = 'A'
+elif(numbers>80):
+    grade = 'B'
+else:
+    grade = 'Do not know'
+print("The grade is", grade)
+
+
+ # AND opreter
+print("Enter Your Marks :)")
+numbers = int(input())
+print(numbers)
+
+if(numbers>90 and numbers<100):
+    grade = 'A'
+elif(numbers>80 and numbers<100):
+    grade = 'B'
+else:
+    grade = 'Do not know'
+print("The grade is", grade)
+
+# OR operter
+print("Enter Your Marks :)")
+numbers = int(input())
+print(numbers)
+
+if(numbers>90 or numbers<100):
+    grade = 'A'
+elif(numbers>80 or numbers<100):
+    grade = 'B'
+else:
+    grade = 'Do not know'
+print("The grade is", grade)
+
+
+
+
+
+# loop
+
+
+print("How many times do yu want to execute")
+no = int(input())
+for i in range(0,no):
+    print(i)
---
+++ tutorial_1.py
@@ -0,0 +1,23 @@
+print("Hello world","Hello world 2", end=" ")
+print("Hello world 3", end=" ")
+print("Hello world 4")
+print("Hello world 5")
+
+# Escape sequence
+print("Hello world \n")
+print("Hello world \'fdsd")
+print("Hello world \r fjkg")
+print("Hello world \t fdsf")
+print("Hello world \b fdsasd")
+print("Hello world \f dsad")
+print("Hello world \000 fds")
+print("Hello world \\ fds")
+
+# variables
+var1 = "Himanshu Chauhan"
+var2 = 4
+var3 = 3.14
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
