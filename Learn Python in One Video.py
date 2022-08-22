2b7816ae42181a136c89a250fe7b7b6ee0a058dd
Himanshu Chauhan <himanshuch8055@gmail.com>
2022-08-23 02:05:45 +0530

    End of the course

--- Learn Python in One Video.py
+++ Learn Python in One Video.py
@@ -238,3 +238,47 @@
 
 # OOPS Object oriented programming
 
+class Employee:
+    __name = None
+    __id = 0
+    __salary = 0
+
+    def __init__(self, name, id, salary):
+        self.__name = name
+        self.__id = id
+        self.__salary = salary
+
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_id(self, id):
+        self.__id = id
+
+    def get_id(self):
+        return self.__id
+
+    def set_salary(self, salary):
+        self.__salary = salary
+
+    def get_salary(self):
+        return self.__salary
+
+# himanshu = Employee()
+# # print(himanshu.__name)
+# himanshu.set_name('himanshu')
+# print(himanshu.get_name())
+
+# himanshu.set_id('himanshuch8055')
+# print(himanshu.get_id())
+
+# himanshu.set_salary('20000000000')
+# print(himanshu.get_salary())
+
+harry = Employee('harry', 489, 70000000)
+print(harry.get_name())
+print(harry.get_id())
+print(harry.get_salary())
