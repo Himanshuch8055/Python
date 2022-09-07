1dfe7082d834aca02676446be7e8167937990622
Himanshu Chauhan <himanshuch8055@gmail.com>
2022-09-08 00:14:06 +0530

    Modified tic tac toe

--- Python Projects/Tic-tac-toe-in-python.py
+++ Python Projects/Tic-tac-toe-in-python.py
@@ -1,1 +1,26 @@
-print("Namste Duniya")
+# print("Namste Duniya")
+
+def printBord():
+    print(f"0 | 1 | 2 ")
+    print(f"--|---|---")
+    print(f"3 | 4 | 5 ")
+    print(f"--|---|---")
+    print(f"6 | 7 | 8 ")
+    pass
+
+if __name__ == "__main__":
+    xState = [0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1for X and for 0
+    print("Welcome to tic tac toe")
+    while(True):
+        if(turn == 1):
+            print("X's Chance")
+            value = int(input("Please enter a value"))
+            xState[value] = 1
+        else:
+            print("X's Chance")
+            value = int(input("Please enter a value"))
+            zState[value] = 1
+        printBord()
+        break
