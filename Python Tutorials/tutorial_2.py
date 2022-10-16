e0655b77cac3d28f8f93355c654f852eedf61d62
Himanshu Chauhan <himanshuch8055@gmail.com>
2022-10-16 12:33:01 +0530

    Python tut_2.py Add

---
+++ Python Tutorials/tutorial_2.py
@@ -0,0 +1,47 @@
+# a = 13
+# b = 33
+# print(a < b)
+# print(a > b)
+# # a -- b is False
+# print(a == b)
+
+# # a! b As True
+# print(a != b)
+
+# # a > b is False
+# print(a >= b)
+
+# # ab is True
+# print(a <= b)
+
+
+# a = int(input(“ Enter the year that you want to check: ”))
+# if (( a%400 == 0)or (( a%4 == 0 ) and ( a%100 != 0))):
+
+#     print(“The year entered %s is a leap year” %a)
+
+# else:
+
+#     print(“The year entered %s is not a leap year” %a)
+
+
+# a = int(input(“ Enter the year that you want to check: ”))
+# if (a% 4) == 0
+
+#     if (a % 100) == 0:
+
+#         if (a % 400) == 0:
+
+#             print(“The year entered %s is a leap year” %a)
+
+#         else:
+
+#             print(“The year entered %s is not a leap year” %a)
+
+#     else:
+
+#         print(“The year entered %s is a leap year” %a)
+
+# else:
+
+#     print(“The year entered %s is not a leap year” %a)
